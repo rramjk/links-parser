a749b53de583aced0507f79a7cbbc6760914ad30
rramjk <rramjk@mail.ru>
2024-03-27 16:22:28 +0400

    add time and param checker

--- main.go
+++ main.go
@@ -1,8 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+)
+
 // test
 
-func main(){
-	fmt.Println("Hello my test!");
+func main() {
+	startProgram()
+}
+
+func startProgram() {
+	startTime := time.Now()
+	var requestSource string
+	var directoryForParse string
+	parseParam(requestSource, directoryForParse)
+
+	fmt.Println(requestSource + " " + directoryForParse)
+
+	fmt.Printf(" Время работы программы: %v\n", time.Now().Sub(startTime))
+}
+
+func parseParam(src string, dst string) {
+	flag.StringVar(&src, "src", "null", "address for request")
+	flag.StringVar(&dst, "dst", "null", "directory for response")
+	flag.Parse()
+	if src == "null" || dst == "null" {
+		fmt.Println(errors.New("Недостаточно параметров!"))
+		os.Exit(0)
+	}
 }
